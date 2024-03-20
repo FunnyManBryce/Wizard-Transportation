428f5dba80197973052d8c73283ff51a688908b4
SplintRcast <emilianojude@gmail.com>
2024-03-20 12:50:45 -0700

    Balls 2

---
+++ Wizard Transportation Github/Assets/Scripts/CharMove.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    public float vertSpeed = 5f;
+    public float horizSpeed = 5f;
+}
