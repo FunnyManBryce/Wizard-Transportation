f53a7df12eb5a2fe92c4450b50cce44da0ce3461
SplintRcast <emilianojude@gmail.com>
2024-03-26 12:33:47 -0700

    Erm balls 5

--- Wizard Transportation Github/Assets/Scripts/GameManager.cs
+++ Wizard Transportation Github/Assets/Scripts/GameManager.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int popularity = 5;
+    public int popularity = 70;
+    public int gold = 100;
+
 
     public PopBar PopularityBar;
 
     private void Start()
     {
         PopularityBar.SetPopularity(popularity);
+    }
+    private void Update()
+    {
+        if (popularity > 100)
+        {
+            popularity = 100;
+            PopularityBar.SetPopularity(popularity);
+        }
     }
 }
