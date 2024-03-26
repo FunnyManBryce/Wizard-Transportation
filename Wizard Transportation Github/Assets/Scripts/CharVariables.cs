17170692a141e606f4e0aae7b1257fd44b6cb2c1
FunnyManBryce <bryce.g.roberts@gmail.com>
2024-03-26 13:45:39 -0700

    I refuse to gritty

--- Wizard Transportation Github/Assets/Scripts/CharVariables.cs
+++ Wizard Transportation Github/Assets/Scripts/CharVariables.cs
@@ -2,7 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharVariables : MonoBehaviour
+ [CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharVariables : ScriptableObject
 {
+    //Real stats
+    public string name;
+    public string species;
+    public float magicLevel;
+    public float adventurerRank;
+    public float age;
+    public string characterClass;
+    public bool isAllowed;
+    public bool isRobot;
+    public string destination;
+
+    //Bag
+    public bool hasBag;
+    public GameObject[] belongings;
+
+    //ID Stats
+    public bool hasID;
+    public string IDName;
+    public string IDSpecies;
+    public string IDAge;
+    public string IDClass;
+
+    //TicketStats
+    public bool hasTicket;
+    public string ticketExpirationDate;
+    public string ticketName;
+    public string ticketDestination;
+
+    //Accepting/Denying Stats
+    public int goldReward;
+    public int reputationReward;
+    public bool isBribing;
+    public int bribeAmount;
+    public string denialReason;
+
+    
     
 }
